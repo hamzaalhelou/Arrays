bc836d48e20e9c860af4ecab0630b67e706bbe66
Hamza AlHelou <125726722+hamzaalhelou@users.noreply.github.com>
2023-07-20 19:27:21 +0300

    Add files via upload

---
+++ Arrays Ex-20221017T160054Z-001/Arrays Ex/src/ArraysExample/ArraysEx.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ArraysExample;
+
+/**
+ *
+ * @author moatasem
+ */
+public class ArraysEx {
+//    
+//      String colors[] = new String[10];
+////      String[] colors = new String[10];         // هذا الأسلوب المفضل في جافا
+//      String colors[] = {"Red", "Black", "Blue", "White"};    // String colors[] = new String[10];
+//    String[] colors = new String[10];         // هذا الأسلوب  إنشاء مصفوفة واعطائها قيم مباشرة
+   
+    //  رقم صفر index  يتم تخزين عناصر المصفوفة إبتداءا من الً
+    //  length-1   بمعنى اخر العنصر الاول في المصفوفه يحمل الاندكس رقم صفر والعنصر الاخير في المصفوفه يحمل الاندكس
+    
+    //   اذا كان نوع المصفوفه انتجر او لونج فإن القيم الافتراضية للمصفوفه ستكون صفر
+    //  0.0  اذا كان نوع المصفوفه فلوت او دبل فإن القيم الافتراضية للمصفوفه ستكون
+    // null اذا كان نوع المصفوفه سترينج فإن القيم الافتراضية للمصفوفه
+   
+    public static void main(String[] args) {
+//
+//        int[] a = { 10, 20, 30, 40, 50 };               // هنا قمنا بتعريف مصفوفة تتألف من خمس  عناصر
+// 
+//        a[0] = 99;                                      // هنا قمنا بتغيير قيمة العنصر الأول
+// 
+//        System.out.println("a[0] = " + a[0]);           // هنا عرضنا قيمة العنصر الأول
+//        System.out.println("a.length = " + a.length );   // هنا عرضنا عدد عناصر المصفوفة
+//      
+     
+      
+      
+//        
+//            هنا قمنا بتعريف المصفوفة
+////       
+//        int[] numbers = new int[5];
+// 
+////          هنا قمنا بعرض قيم جميع عناصرها
+//        for(int i=0; i<numbers.length; i++)   //  زود واحد على طول المصفوفه وليس على قيمة المصفوفه
+//        {
+//            System.out.println("numbers[" + i + "]: " + numbers[i]);
+//        }
+////
+//// 
+////        //  هنا قمنا يتغيير قيم جميع عناصرها
+//        numbers[0] = 1000;
+//        numbers[1] = 2000;
+//        numbers[2] = 3000;
+//        numbers[3] = 4000;
+//        numbers[4] = 5000;
+////// 
+////        System.out.println();   //  فقط لعمل سطر جديد
+//////
+//////        //  هنا قمنا بعرض قيم جميع عناصرها
+////        for(int i=0; i<numbers.length; i++)
+////        {
+////            System.out.println("numbers[" + i + "]: " + numbers[i]);
+////        }
+//      
+//  
+//     
+//     //   foreach
+//        
+//     //   for( المتغير : المصفوفه ) {
+//     //   statements
+//     //    }
+//       
+//   
+//        // هنا قمنا بتعريف المصفوفة
+////        int[] numbers = { 1000, 2000, 3000, 4000, 5000 };
+//// 
+////        // index في المتغير numbers هنا في كل دورة سيتم تخزين قيمة عنصر من عناصر المصفوفة
+////        for(int index : numbers)
+////        {
+////            System.out.println(index);   // element هنا سيتم عرض القيمة التي تخزنت في المتغير
+////        }
+//
+//  
+   }
+}
